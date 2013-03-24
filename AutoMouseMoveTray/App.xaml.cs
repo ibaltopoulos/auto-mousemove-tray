fff9cf3e99654bf1420fa02ca45e6917d126bfc8
ibaltopoulos <ioannis.baltopoulos@gmail.com>
2013-03-24 19:43:39 +0000

    Added a tray icon. Added logic that closes the application when one right-clicks the tray icon.

--- AutoMouseMoveTray/App.xaml.cs
+++ AutoMouseMoveTray/App.xaml.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Windows;
+﻿using System.Windows;
+using System.Windows.Forms;
+using Application = System.Windows.Application;
 
 namespace AutoMouseMoveTray
 {
@@ -12,5 +9,31 @@ namespace AutoMouseMoveTray
     /// </summary>
     public partial class App : Application
     {
+        private readonly NotifyIcon _notifyIcon = new NotifyIcon();
+        
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e); 
+
+            _notifyIcon.Icon = AutoMouseMoveTray.Properties.Resources.TrayIcon;// new Icon(@"../../TrayIcon.ico");
+            _notifyIcon.ShowBalloonTip(5000, "Hi", "This is a BallonTip from Windows Notification", ToolTipIcon.Info);
+            _notifyIcon.Visible = true;
+
+            _notifyIcon.MouseClick += _notifyIcon_MouseClick;
+        }
+
+        void _notifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right) {
+                Current.Shutdown();
+            }
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            _notifyIcon.Visible = false;
+            _notifyIcon.Dispose();
+        } 
     }
 }
